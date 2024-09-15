38cd39c4f1f680201b9d9966fea5242ac85f5bde
LithiumEngineer <haochenkang@gmail.com>
2024-09-15 00:27:42 -0400

    chatbot

--- frontend/src/components/home/index.js
+++ frontend/src/components/home/index.js
@@ -1,13 +1,11 @@
-import { Input, Popover } from "antd"
+import { ShoppingCartOutlined } from "@ant-design/icons"
+import { Button, Divider, Input, List, Modal, Popover, Skeleton } from "antd"
 import Groq from "groq-sdk"
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { IoIosCheckmark } from "react-icons/io"
+import { RiRobot2Line } from "react-icons/ri"
+import InfiniteScroll from "react-infinite-scroll-component"
 import Typewriter from "typewriter-effect"
-import { Button, Modal } from 'antd';
-import { ShoppingCartOutlined } from '@ant-design/icons';
-import { useEffect } from 'react';
-import { Avatar, Divider, List, Skeleton } from 'antd';
-import InfiniteScroll from 'react-infinite-scroll-component';
 
 const Home = () => {
   // const { user } = useAuth0()
@@ -26,37 +24,39 @@ const Home = () => {
     setOptionsSelected(optionsSelected.map((t, j) => (i === j ? true : t)))
   }
   // shared cart
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false)
   const showModal = () => {
-    setIsModalOpen(true);
-  };
+    setIsModalOpen(true)
+  }
   const handleOk = () => {
-    setIsModalOpen(false);
-  };
+    setIsModalOpen(false)
+  }
   const handleCancel = () => {
-    setIsModalOpen(false);
-  };
+    setIsModalOpen(false)
+  }
   // shared cart table scroll stuff
-  const [loading, setLoading] = useState(false);
-  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false)
+  const [data, setData] = useState([])
   const loadMoreData = () => {
     if (loading) {
-      return;
+      return
     }
-    setLoading(true);
-    fetch('https://randomuser.me/api/?results=10&inc=name,gender,email,nat,picture&noinfo')
+    setLoading(true)
+    fetch(
+      "https://randomuser.me/api/?results=10&inc=name,gender,email,nat,picture&noinfo"
+    )
       .then((res) => res.json())
       .then((body) => {
-        setData([...data, ...body.results]);
-        setLoading(false);
+        setData([...data, ...body.results])
+        setLoading(false)
       })
       .catch(() => {
-        setLoading(false);
-      });
-  };
+        setLoading(false)
+      })
+  }
   useEffect(() => {
-    loadMoreData();
-  }, []);
+    loadMoreData()
+  }, [])
 
   const processString = (s) => {
     for (let i = s.length - 1; i >= 0; i--) {
@@ -68,14 +68,23 @@ const Home = () => {
   }
 
   async function askGroq() {
-    const chatCompletion = await getGroqChatCompletion()
-    setChatHistory([
-      ...(chatHistory ?? []),
-      processString(chatCompletion.choices[0]?.message?.content || "")?.[0],
-    ])
-    setOptions(
-      processString(chatCompletion.choices[0]?.message?.content || "")?.[1]
-    )
+    try {
+      const chatCompletion = await getGroqChatCompletion()
+      setChatHistory((chatHistory) => [
+        ...(chatHistory ?? []),
+        {
+          value: processString(
+            chatCompletion.choices[0]?.message?.content || ""
+          )?.[0],
+          type: "gpt",
+        },
+      ])
+      setOptions(
+        processString(chatCompletion.choices[0]?.message?.content || "")?.[1]
+      )
+    } catch (error) {
+      console.log("error: " + error)
+    }
   }
 
   async function getGroqChatCompletion() {
@@ -92,11 +101,15 @@ const Home = () => {
     })
   }
 
+  // useEffect(() => {
+  //   console.log(chatHistory)
+  // }, [chatHistory])
+
   return (
-    <div className="flex flex-col bg-gray-300 min-h-screen">
+    <div className="flex flex-col bg-slate-50 min-h-screen">
       <div className="flex-1 overflow-auto">
         <div className="flex justify-between">
-            {/* <svg
+          {/* <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
@@ -110,17 +123,25 @@ const Home = () => {
                 d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
               />
             </svg> */}
-          <Button onClick={showModal} className="ml-4 mt-4 h-12 bg-grey shadow-lg">
-            <ShoppingCartOutlined style={{ fontSize: '200%'}}/>
+          <Button
+            onClick={showModal}
+            className="ml-4 mt-4 h-12 bg-grey shadow-lg"
+          >
+            <ShoppingCartOutlined style={{ fontSize: "200%" }} />
           </Button>
-          <Modal title="Shopping Cart" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+          <Modal
+            title="Shopping Cart"
+            open={isModalOpen}
+            onOk={handleOk}
+            onCancel={handleCancel}
+          >
             <div
               id="scrollableDiv"
               style={{
                 height: 400,
-                overflow: 'auto',
-                padding: '0 16px',
-                border: '1px solid rgba(140, 140, 140, 0.35)',
+                overflow: "auto",
+                padding: "0 16px",
+                border: "1px solid rgba(140, 140, 140, 0.35)",
               }}
             >
               <InfiniteScroll
@@ -181,71 +202,88 @@ const Home = () => {
             </svg>
           </Popover>
         </div>
-
-        {chatHistory?.map((c, i) => (
-          <div className="mt-10 w-3/4 flex flex-col items-center justify-around ml-40 text-xl">
-            {i === chatHistory.length - 1 ? (
-              <>
-                <div className="w-full">
-                  <Typewriter
-                    onInit={(typewriter) => {
-                      typewriter
-                        .typeString(c)
-                        .callFunction(() => {
-                          setShowOptions(true)
-                          setOptionsSelected([false, false, false])
-                        })
-                        .start()
-                    }}
-                    options={{
-                      autoStart: true,
-                      delay: 5,
-                      cursor: null,
-                      loop: false,
-                      deleteSpeed: 10000,
-                    }}
-                  />
+        <div className="flex flex-col items-center max-h-[680px] overflow-y-scroll">
+          {chatHistory?.map((c, i) => (
+            <div className="flex items-end w-3/4 justify-around">
+              {c.type === "gpt" && (
+                <div className="w-10 h-10 bg-gray-400 mr-4 rounded-full flex justify-around items-center text-white mb-4">
+                  <RiRobot2Line />
                 </div>
-                {showOptions && (
-                  <div className="flex w-fit space-x-10 mt-4">
-                    {options?.map((o, i) => (
-                      <div
-                        className={`flex items-center justify-around text-center text-slate-800 text-lg w-40 h-40 bg-slate-50 border-[1px] border-solid border-slate-300 rounded-lg duration-200 ${
-                          optionsSelected[i]
-                            ? "border-green-500 bg-white"
-                            : "hover:bg-slate-100 hover:cursor-pointer hover:border-green-500"
-                        }`}
-                        onClick={() => {
-                          onAddToCart(i)
+              )}
+              <div className={`flex-1 mt-2 w-3/4 flex flex-col items-center justify-around text-xl rounded-xl border-[1px] border-solid border-slate-200 p-6 ${c.type === "gpt" ? "bg-slate-100" : "bg-white"}`}>
+                {i === chatHistory.length - 1 && c.type === "gpt" ? (
+                  <>
+                    <div className="w-full">
+                      <Typewriter
+                        onInit={(typewriter) => {
+                          typewriter
+                            .typeString(c.value)
+                            .callFunction(() => {
+                              setShowOptions(true)
+                              setOptionsSelected([false, false, false])
+                            })
+                            .start()
                         }}
-                      >
-                        {optionsSelected[i] ? (
-                          <IoIosCheckmark className="text-green-500 h-20 w-20" />
-                        ) : (
-                          o
-                        )}
+                        options={{
+                          autoStart: true,
+                          delay: 5,
+                          cursor: null,
+                          loop: false,
+                          deleteSpeed: 10000,
+                        }}
+                      />
+                    </div>
+                    {showOptions && (
+                      <div className="flex w-fit space-x-10 mt-4">
+                        {options?.map((o, i) => (
+                          <div
+                            className={`flex items-center justify-around text-center text-slate-800 text-lg w-40 h-40 bg-slate-50 border-[1px] border-solid border-slate-300 rounded-lg duration-200 ${
+                              optionsSelected[i]
+                                ? "border-green-500 bg-white"
+                                : "hover:bg-slate-100 hover:cursor-pointer hover:border-slate-500"
+                            }`}
+                            onClick={() => {
+                              onAddToCart(i)
+                            }}
+                          >
+                            {optionsSelected[i] ? (
+                              <IoIosCheckmark className="text-green-500 h-20 w-20" />
+                            ) : (
+                              o
+                            )}
+                          </div>
+                        ))}
                       </div>
-                    ))}
-                  </div>
+                    )}
+                  </>
+                ) : (
+                  <>
+                    <div className="w-full">{c.value}</div>
+                  </>
                 )}
-              </>
-            ) : (
-              <>
-                <div className="w-full">{c}</div>
-              </>
-            )}
-          </div>
-        ))}
+              </div>
+              {c.type === "user" && (
+                <div className="w-10 h-10 bg-green-400 ml-4 rounded-full flex justify-around items-center text-white mb-4">
+                  M
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
       </div>
       <div className="py-8 px-4 flex justify-center">
         <Input
           placeholder="Enter your request"
-          className="md:w-2/3 1/2 shadow-lg"
+          className="h-10 w-1/2 shadow-lg"
           value={input}
           onChange={(e) => setInput(e.target.value)}
-          onKeyDown={(e) => {
+          onKeyDown={async (e) => {
             if (e.key === "Enter") {
               setInput("")
+              setChatHistory((chatHistory) => [
+                ...(chatHistory ?? []),
+                { value: input, type: "user" },
+              ])
               askGroq()
             }
           }}
