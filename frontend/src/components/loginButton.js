b60c15f367704ddb021281ee64663a7d3b97f07a
Charlie Li <charlie.li711935@gmail.com>
2024-09-15 02:41:39 -0400

    one button auth

--- frontend/src/components/loginButton.js
+++ frontend/src/components/loginButton.js
@@ -4,7 +4,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 const LoginButton = () => {
   const { loginWithRedirect, isAuthenticated } = useAuth0();
 
-  return (<button onClick={() => loginWithRedirect()} className="text-white hover:text-gray-300">Log In</button>);
+  return (!isAuthenticated && <button onClick={() => loginWithRedirect()} className="text-black hover:text-gray-300">Log In</button>);
 };
 
 export default LoginButton;
